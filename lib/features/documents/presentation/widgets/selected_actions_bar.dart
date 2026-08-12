781f3bb0e0da649be13f6abe5f4ce0a9f7fa8f85
Roman Kajgorodcev <2005spain@gmail.com>
2026-08-13 02:09:22 +0300

    end botto, select buttos

--- lib/features/documents/presentation/widgets/selected_actions_bar.dart
+++ lib/features/documents/presentation/widgets/selected_actions_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
 import '../../../../app/theme/app_colors.dart';
+import 'circle_icon_button.dart';
 
 class SelectedActionsBar extends StatelessWidget {
   final VoidCallback onDelete;
@@ -12,36 +12,14 @@ class SelectedActionsBar extends StatelessWidget {
     return Positioned(
       left: 18,
       right: 18,
-      bottom: 26,
+      bottom: 34,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          _FloatingAction(icon: Icons.delete_outline, color: AppColors.destructive, onTap: onDelete),
-          _FloatingAction(icon: Icons.ios_share, color: AppColors.textPrimary, onTap: () {}),
+          CircleIconButton(assetPath: 'assets/figma/trash.svg', onPressed: onDelete, foregroundColor: AppColors.destructive, size: 62, iconSize: 22, semanticLabel: 'Delete'),
+          CircleIconButton(assetPath: 'assets/figma/share.svg', onPressed: () {}, foregroundColor: AppColors.textPrimary, size: 62, iconSize: 22, semanticLabel: 'Share'),
         ],
       ),
-    );
-  }
-}
-
-class _FloatingAction extends StatelessWidget {
-  final IconData icon;
-  final Color color;
-  final VoidCallback onTap;
-
-  const _FloatingAction({required this.icon, required this.color, required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return GlassIconButton(
-      icon: Icon(icon, color: color),
-      onPressed: onTap,
-      size: 58,
-      iconSize: 30,
-      useOwnLayer: true,
-      interactionScale: 0.94,
-      glowColor: AppColors.white.withValues(alpha: 0.42),
-      glowRadius: 20,
     );
   }
 }
